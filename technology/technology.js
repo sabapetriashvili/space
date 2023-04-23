fe72f54e0ffa111aee8816072310e3a2c4c9610b
Saba <sabapetriashvili@gmail.com>
2023-04-24 01:14:49 +0400

    Add files via upload

--- technology/technology.js
+++ technology/technology.js
@@ -50,14 +50,18 @@ infoIndicators.forEach(indicator => {
 
       if(window.screen.width > 1065) {
         technologyImg.style.backgroundImage = `url('${technology[indexOfIndicator].images.portrait}')`;
+        technologyImg.addEventListener('load', () => {
+          technologyImg.style.animation = 'techno-img 2s';
+        });
       } else {
         technologyImg.style.backgroundImage = `url('${technology[indexOfIndicator].images.landscape}')`;
+        technologyImg.addEventListener('load', () => {
+          technologyImg.style.animation = 'techno-img 2s';
+        });
       }
 
-      technologyImg.addEventListener('load', () => {
-        technologyImg.style.animation = 'techno-img 2s';
-      });
-      
+ 
+
       technologyName.innerHTML = technology[indexOfIndicator].name;
       technologyInfo.style.animation = 'techno 2s';
 
